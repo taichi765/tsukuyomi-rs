743793903f27aee1aee2fba4eca6791efa624a94
taichi765 <taichi0209.youtub@gmail.com>
2025-11-23 10:16:44 +0900

    core: Implement CommandManager

--- core/src/commands.rs
+++ core/src/commands.rs
@@ -1,3 +1,47 @@
+use crate::doc::{Doc, DocCommand};
+
 pub mod doc;
 pub mod static_scene;
 pub mod timeline;
+
+pub struct CommandManager {
+    commands: Vec<Box<dyn DocCommand>>,
+    current_index: usize,
+}
+
+impl CommandManager {
+    pub fn new() -> Self {
+        Self {
+            commands: Vec::new(),
+            current_index: 0,
+        }
+    }
+
+    pub fn execute(
+        &mut self,
+        mut command: Box<dyn DocCommand>,
+        doc: &mut Doc,
+    ) -> Result<(), String> {
+        command.apply(doc)?;
+        self.commands.truncate(self.current_index);
+        self.commands.push(command);
+        self.current_index = self.commands.len();
+        Ok(())
+    }
+
+    pub fn undo(&mut self, doc: &mut Doc) -> Result<(), String> {
+        if self.current_index == 0 {
+            return Err("no command to undo".into());
+        }
+        self.current_index -= 1;
+        self.commands[self.current_index].revert(doc)
+    }
+
+    pub fn redo(&mut self, doc: &mut Doc) -> Result<(), String> {
+        if self.current_index == self.commands.len() {
+            return Err("no command to redo".into());
+        }
+        self.current_index += 1;
+        self.commands[self.current_index].apply(doc)
+    }
+}
--- core/src/commands/doc.rs
+++ core/src/commands/doc.rs
@@ -2,9 +2,12 @@ use uuid::Uuid;
 
 use crate::{
     doc::{Doc, DocCommand},
+    fixture::Fixture,
+    fixture_def::FixtureDef,
     functions::FunctionData,
 };
-
+// TODO: エラー型の定義(enum or trait)
+// TODO: 重複部分のマクロ定義？
 pub struct AddFunction {
     function: Option<FunctionData>,
     function_id: Uuid,
@@ -22,8 +25,7 @@ impl AddFunction {
 impl DocCommand for AddFunction {
     fn apply(&mut self, doc: &mut Doc) -> Result<(), String> {
         if let Some(f) = self.function.take() {
-            doc.add_function(f);
-            Ok(())
+            doc.add_function(f)
         } else {
             Err("function is already moved".into())
         }
@@ -34,6 +36,73 @@ impl DocCommand for AddFunction {
             Ok(())
         } else {
             Err("function is already removed from doc".into())
+        }
+    }
+}
+
+pub struct AddFixture {
+    fixture: Option<Fixture>,
+    fixture_id: Uuid,
+}
+
+impl AddFixture {
+    pub fn new(fixture: Fixture) -> Self {
+        Self {
+            fixture_id: fixture.id(),
+            fixture: Some(fixture),
+        }
+    }
+}
+
+impl DocCommand for AddFixture {
+    fn apply(&mut self, doc: &mut Doc) -> Result<(), String> {
+        if let Some(f) = self.fixture.take() {
+            doc.add_fixture(f);
+            Ok(())
+        } else {
+            Err("fixture is already moved".into())
+        }
+    }
+
+    fn revert(&mut self, doc: &mut Doc) -> Result<(), String> {
+        if let Some(f) = doc.remove_fixture(self.fixture_id) {
+            self.fixture = Some(f);
+            Ok(())
+        } else {
+            Err("fixture is already removed".into())
+        }
+    }
+}
+
+pub struct AddFixtureDef {
+    fixture_def_id: Uuid,
+    fixture_def: Option<FixtureDef>,
+}
+
+impl AddFixtureDef {
+    pub fn new(fixture_def: FixtureDef) -> Self {
+        Self {
+            fixture_def_id: fixture_def.id(),
+            fixture_def: Some(fixture_def),
+        }
+    }
+}
+
+impl DocCommand for AddFixtureDef {
+    fn apply(&mut self, doc: &mut Doc) -> Result<(), String> {
+        if let Some(def) = self.fixture_def.take() {
+            doc.add_fixture_def(def);
+            Ok(())
+        } else {
+            Err("fixture definition is already moved".into())
+        }
+    }
+    fn revert(&mut self, doc: &mut Doc) -> Result<(), String> {
+        if let Some(def) = doc.remove_fixture_def(self.fixture_def_id) {
+            self.fixture_def = Some(def);
+            Ok(())
+        } else {
+            Err("fixture definition is arleady removed".into())
         }
     }
 }
