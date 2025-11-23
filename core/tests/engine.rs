46bcea94d8c3874a335221182148f895af533a1c
taichi765 <taichi0209.youtub@gmail.com>
2025-11-23 10:17:16 +0900

    test: Update to use CommandManager

--- core/tests/engine.rs
+++ core/tests/engine.rs
@@ -1,37 +1,100 @@
 use std::{
+    collections::HashMap,
     sync::{Arc, mpsc},
     thread,
     time::Duration,
 };
 
 use tsukuyomi_core::{
+    commands::{self, CommandManager},
     doc::Doc,
     engine::{Engine, EngineCommand},
-    functions::{FunctionDataGetters, SceneValue, StaticSceneData},
+    fixture::{Fixture, MergeMode},
+    fixture_def::{ChannelDef, FixtureDef, FixtureMode},
+    functions::{FunctionData, FunctionDataGetters, SceneValue, StaticSceneData},
+    plugins::Plugin,
+    universe::{DmxAddress, UniverseId},
 };
-use uuid::Uuid;
+
+struct MockPlugin {}
+
+impl Plugin for MockPlugin {
+    fn send_dmx(&self, universe_id: u8, dmx_data: &[u8]) -> Result<(), std::io::Error> {
+        println!("{universe_id}: {}", dmx_data[0]);
+        Ok(())
+    }
+}
 
 #[test]
 fn engine_can_start_function() {
     let mut doc = Doc::new();
-    let fixture_id = Uuid::new_v4();
+    let mut command_manager = CommandManager::new();
+
+    let mut fixture_def = FixtureDef::new("Test Manufacturer".into(), "Test Model".into());
+    let mut mode: HashMap<String, Option<(usize, ChannelDef)>> = HashMap::new();
+    mode.insert(
+        "Dimmer".into(),
+        Some((
+            0,
+            ChannelDef {
+                merge_mode: MergeMode::HTP,
+            },
+        )),
+    );
+    mode.insert(
+        "Red".into(),
+        Some((
+            1,
+            ChannelDef {
+                merge_mode: MergeMode::HTP,
+            },
+        )),
+    );
+    let mode = FixtureMode {
+        channel_order: mode,
+    };
+    fixture_def.add_mode("Mode 1".into(), mode);
+    command_manager.execute(
+        Box::new(commands::doc::AddFixtureDef::new(fixture_def)),
+        &mut doc,
+    );
+
+    let fixture = Fixture::new(
+        "Fixture",
+        UniverseId::new(0),
+        DmxAddress::new(0).unwrap(),
+        fixture_def_id,
+        "Mode 1".into(),
+    );
+
+    command_manager.execute(Box::new(commands::doc::AddFixture::new(fixture)), &mut doc);
 
     let mut scene = StaticSceneData::new("My Scene");
     let mut sv = SceneValue::new();
     sv.insert("Dimmer".into(), 200);
     sv.insert("Red".into(), 100);
     scene.insert_value(fixture_id, sv);
-
     let scene_id = scene.id();
 
-    //doc.add_function(FunctionData::StaticScene(scene)).unwrap();
+    command_manager.execute(
+        Box::new(commands::doc::AddFunction::new(FunctionData::StaticScene(
+            scene,
+        ))),
+        &mut doc,
+    );
 
     let (command_tx, command_rx) = mpsc::channel::<EngineCommand>();
-    let mut engine = Engine::new(Arc::new(doc), command_rx);
+    let engine = Engine::new(Arc::new(doc), command_rx);
 
     let engine_handle = thread::Builder::new()
         .name("tsukuyomi-engine".into())
         .spawn(move || engine.start_loop())
+        .unwrap();
+
+    command_tx.send(EngineCommand::AddUniverse).unwrap();
+
+    command_tx
+        .send(EngineCommand::AddPlugin(Box::new(MockPlugin {})))
         .unwrap();
 
     command_tx
