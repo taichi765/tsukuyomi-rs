317f7618c5defe056e656bd1ac1b685214324d5c
taichi765 <taichi0209.youtub@gmail.com>
2025-12-04 14:32:54 +0900

    refactor(ui): Create `run_main()` in lib.rs
    Keep main.rs simple

--- slint/src/lib.rs
+++ slint/src/lib.rs
@@ -4,18 +4,24 @@ pub mod fader_view_bridge;
 pub mod preview_plugin;
 
 use std::collections::HashMap;
+use std::error::Error;
+use std::rc::Rc;
 use std::sync::mpsc::Sender;
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, RwLock, mpsc};
 use std::thread;
 use std::time::Duration;
 
+use i_slint_backend_winit::WinitWindowAccessor;
+
 use slint::Weak;
-use tsukuyomi_core::engine::EngineCommand;
+use slint::{Brush, Color, VecModel};
+use tsukuyomi_core::command_manager::CommandManager;
+use tsukuyomi_core::engine::{Engine, EngineCommand, EngineMessage};
 use tsukuyomi_core::functions::FunctionId;
 use tsukuyomi_core::{
     commands,
     commands::DocCommand,
-    doc::Doc,
+    doc::{Doc, DocObserver},
     fixture::{Fixture, MergeMode},
     fixture_def::{ChannelDef, FixtureDef, FixtureMode},
     functions::{FunctionData, FunctionDataGetters, SceneValue, StaticSceneData},
@@ -24,14 +30,80 @@ use tsukuyomi_core::{
 };
 
 use crate::bottom_panel_bridge::BottomPanelBridge;
+use crate::doc_event_bridge::DocEventBridge;
+use crate::fader_view_bridge::{adapt_2d_preview, adapt_fader_view};
 use crate::preview_plugin::PreviewOutput;
 // TODO: tsukuyomi_core::prelude使いたい
 
 slint::include_modules!();
 
-pub fn init_bridges(ui: Weak<AppWindow>, doc: Arc<RwLock<Doc>>) {
-    let ui = ui.unwrap();
-    let bridge = BottomPanelBridge::new(ReadOnly::new(doc));
+pub fn run_main() -> Result<(), Box<dyn Error>> {
+    let mut doc = Doc::new();
+    let mut command_manager = CommandManager::new();
+
+    let (commands, scene_id) = create_some_presets();
+    commands
+        .into_iter()
+        .for_each(|cmd| command_manager.execute(cmd, &mut doc).unwrap());
+
+    let (command_tx, command_rx) = mpsc::channel::<EngineCommand>();
+    let (error_tx, error_rx) = mpsc::channel::<EngineMessage>();
+
+    let doc_event_bridge: Arc<RwLock<dyn DocObserver>> =
+        Arc::new(RwLock::new(DocEventBridge::new(command_tx.clone())));
+    doc.subscribe(Arc::downgrade(&doc_event_bridge));
+
+    let doc = Arc::new(RwLock::new(doc));
+    let engine = Engine::new(ReadOnly::new(Arc::clone(&doc)), command_rx, error_tx);
+
+    let engine_handle = thread::Builder::new()
+        .name("tsukuyomi-engine".into())
+        .spawn(move || engine.start_loop())
+        .unwrap();
+
+    try_some_commands(command_tx.clone(), scene_id);
+
+    let ui = AppWindow::new()?;
+    // TODO: language switch(preferences)
+    slint::select_bundled_translation("en".into()).unwrap();
+
+    let ui_handle = ui.as_weak();
+    ui.on_start_drag(move || {
+        let ui = ui_handle.unwrap();
+        ui.window().with_winit_window(|w| w.drag_window());
+    });
+
+    let ui_handle = ui.as_weak();
+    ui.on_minimize(move || {
+        let ui = ui_handle.unwrap();
+        ui.window().set_minimized(true);
+    });
+
+    // TODO: toggle-fullscreen
+
+    let ui_handle = ui.as_weak();
+    ui.on_close(move || {
+        let ui = ui_handle.unwrap();
+        ui.window().hide().unwrap()
+    });
+
+    #[cfg(target_os = "macos")]
+    ui.set_is_macos(true);
+    #[cfg(not(target_os = "macos"))]
+    ui.set_is_macos(false);
+
+    adapt_fader_view(&ui, command_tx.clone());
+    adapt_2d_preview(&ui);
+
+    ui.run()?;
+
+    if let Err(e) = command_tx.send(EngineCommand::Shutdown) {
+        eprintln!("failed to send message to engine:{}", e);
+    }
+
+    engine_handle.join().unwrap();
+
+    Ok(())
 }
 
 pub fn create_some_presets() -> (Vec<Box<dyn DocCommand>>, FunctionId) {
@@ -105,5 +177,4 @@ pub fn try_some_commands(command_tx: Sender<EngineCommand>, scene_id: FunctionId
     command_tx
         .send(EngineCommand::StopFunction(scene_id))
         .unwrap();
-    command_tx.send(EngineCommand::Shutdown).unwrap();
 }
