05b6746cda1535d662ad67cb019207459b92b843
taichi765 <taichi0209.youtub@gmail.com>
2025-12-08 20:41:07 +0900

    chore: Update Cargo.toml and build.rs to reflect directory name

--- ui/build.rs
+++ ui/build.rs
@@ -2,5 +2,5 @@ fn main() {
     let config = slint_build::CompilerConfiguration::new()
         .with_style("fluent-dark".into())
         .with_bundled_translations("translations");
-    slint_build::compile_with_config("ui/app-window.slint", config).expect("Slint build failed");
+    slint_build::compile_with_config("slint/app-window.slint", config).expect("Slint build failed");
 }
