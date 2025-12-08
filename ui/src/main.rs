91cf55f07f5d2c524e6d8934aed7fa3a7054fb17
taichi765 <taichi0209.youtub@gmail.com>
2025-12-09 08:56:54 +0900

    chore: Rename translation file　and import to reflect package name

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -2,6 +2,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tsukuyomi_slint::run_main()?;
+    tsukuyomi_ui::run_main()?;
     Ok(())
 }
