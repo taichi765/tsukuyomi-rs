103c7e784321ad941f3af9f9ae71226c38b50a8c
taichi765 <taichi0209.youtub@gmail.com>
2025-12-11 13:50:18 +0900

    refactor: Reflect changes around Doc
    - `CommandManager` owns `DocHandle`.
    - Mutable access to Doc is limited by ReadOnly<Doc>.

--- core/src/commands.rs
+++ core/src/commands.rs
@@ -1,11 +1,11 @@
-use crate::doc::Doc;
-
-pub trait DocCommand {
-    fn apply(&mut self, doc: &mut Doc) -> Result<(), String>;
-
-    fn revert(&mut self, doc: &mut Doc) -> Result<(), String>;
-}
-
 pub mod doc_commands;
 pub mod static_scene_commands;
 pub mod timeline_commands;
+
+use crate::doc::DocHandle;
+
+pub trait DocCommand {
+    fn apply(&mut self, doc: &DocHandle) -> Result<(), String>;
+
+    fn revert(&mut self, doc: &DocHandle) -> Result<(), String>;
+}
