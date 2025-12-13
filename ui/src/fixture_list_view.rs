fc7ebd014f909a9214499bb6cabe838818318c85
taichi765 <taichi0209.youtub@gmail.com>
2025-12-13 21:08:19 +0900

    refactor: Simplify implementation of handling DocEvent::FixtureDefInserted

--- ui/src/fixture_list_view.rs
+++ ui/src/fixture_list_view.rs
@@ -42,50 +42,46 @@ impl FixtureListViewController {
 impl DocObserver for FixtureListViewController {
     fn on_doc_event(&mut self, event: &DocEvent) {
         match event {
-            DocEvent::FixtureDefInserted(id) => {
+            DocEvent::FixtureDefInserted(def_id) => {
                 let ui = self.ui_handle.unwrap();
                 let store = ui.global::<FixtureListStore>();
-
                 let doc = self.doc.read();
-                let def = doc.get_fixture_def(id).unwrap();
+
+                let def = doc.get_fixture_def(def_id).unwrap();
+                let def_id = def_id.to_shared_string();
+
                 let new_manufacturer = def.manufacturer();
                 let new_fixture_model = FixtureModel {
-                    id: id.to_shared_string(),
+                    id: def_id.to_shared_string(),
                     name: def.model().to_shared_string(),
+                    modes: Rc::new(VecModel::from(
+                        def.modes()
+                            .keys()
+                            .map(|s| s.to_shared_string())
+                            .collect::<Vec<_>>(),
+                    ))
+                    .into(),
                 };
-                let mut manufactures: Vec<ManufacturerModel> = store.get_model().iter().collect();
-                let mut is_updated = false;
-                for m in &mut manufactures {
-                    if m.manufacturer != new_manufacturer {
-                        continue;
-                    }
-                    let filtered: Vec<(usize, FixtureModel)> = m
-                        .fixtures
-                        .iter()
-                        .enumerate()
-                        .filter(|(_, fxt)| fxt.id == id.to_shared_string())
-                        .collect();
-                    if filtered.len() != 0 {
-                        m.fixtures
-                            .set_row_data(filtered[0].0, new_fixture_model.clone());
-                        is_updated = true;
-                    } else {
-                        m.fixtures
-                            .set_row_data(m.fixtures.iter().len(), new_fixture_model.clone());
-                        is_updated = true;
-                    }
-                    break;
-                }
-                if !is_updated {
-                    manufactures.push(ManufacturerModel {
+
+                let mut manufacturers: Vec<ManufacturerModel> = store.get_model().iter().collect();
+                if let Some(m) = manufacturers
+                    .iter()
+                    .find(|m| m.manufacturer == new_manufacturer)
+                {
+                    let existing_idx = m.fixtures.iter().position(|fxt| fxt.id == def_id);
+                    let idx = existing_idx.unwrap_or_else(|| m.fixtures.iter().count());
+                    m.fixtures.set_row_data(idx, new_fixture_model);
+                } else {
+                    manufacturers.push(ManufacturerModel {
                         expanded: false,
                         fixtures: Rc::new(VecModel::from(vec![new_fixture_model])).into(),
                         manufacturer: new_manufacturer.to_shared_string(),
                     });
                 }
-                store.set_model(Rc::new(VecModel::from(manufactures)).into());
+
+                store.set_model(Rc::new(VecModel::from(manufacturers)).into());
             }
-            DocEvent::FixtureDefRemoved(id) => {}
+            DocEvent::FixtureDefRemoved(id) => todo!(),
             _ => (),
         }
     }
